980f7c19c594f40118de833bf2285bf35c4b36c3
Himannshu121203 <himanshu1571.be20@chitkara.edu.in>
2022-11-03 22:46:49 +0530

    new

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function(){
+    return(
+        <div>
+            <nav>
+                <img src="../images/Airbnb-logo.jpeg" />
+            </nav>
+        </div>
+    )
+}
